5daa5a72da68cd4e9fe4153446f58e0868d63c6b
蘇嘎嘎 <sbj455442@gmail.com>
2016-08-12 21:01:59 +0800

    Show Project details in modal

--- javascripts/config.js
+++ javascripts/config.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
+	// Toggle navigation menu when small screen. 
 	$(".navbar-button").on("click", function() {
 		$("body").toggleClass("opennavbar");
 	});
 	
+	// Set collapse open or close by screen width.
 	var windowWidth = $(window).width();
 	if( windowWidth <= 991 ) {
 		$(".panel-title a").attr("aria-expanded", false);
@@ -11,4 +13,13 @@ $(document).ready(function() {
 		$(".panel-title a").attr("aria-expanded", true);
 		$(".panel-collapse").addClass("in");
 	}
+
+	// For WORKS Project modal.
+    $("a[data-target='#projectModalLG']").on("click", function(ev) {
+        ev.preventDefault();
+        var target = $(this).attr("href");
+        $("#projectModalLG .modal-content").load(target, function() {
+            $("#projectModalLG").modal("show");
+        });
+    });
 });
