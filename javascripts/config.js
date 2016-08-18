72d014762d0a0a04e9df8290afdaa0d360772606
蘇嘎嘎 <sbj455442@gmail.com>
2016-08-18 19:29:10 +0800

    Update for work ui.

--- javascripts/config.js
+++ javascripts/config.js
@@ -1,20 +1,24 @@
 $(document).ready(function() {
-	// Toggle navigation menu when small screen. 
-	$(".navbar-button").on("click", function() {
-		$("body").toggleClass("opennavbar");
-	});
-	
-	// Set collapse open or close by screen width.
-	var windowWidth = $(window).width();
-	if( windowWidth <= 991 ) {
-		$(".panel-title a").attr("aria-expanded", false);
-		$(".panel-collapse").removeClass("in");
-	} else {
-		$(".panel-title a").attr("aria-expanded", true);
-		$(".panel-collapse").addClass("in");
-	}
+    // Toggle navigation menu when small screen. 
+    $(".previous-button").on("click", function() {
+        window.history.back();
+    });
+    $(".navmenu-toggle").on("click", function() {
+        $("body").toggleClass("opennavbar");
+    });
 
-	// For WORKS Project modal.
+    // Set collapse open or close by screen width.
+    var windowWidth = $(window).width(),
+        windowHeight = $(window).height();
+    if (windowWidth <= 991) {
+        $(".panel-title a").attr("aria-expanded", false);
+        $(".panel-collapse").removeClass("in");
+    } else {
+        $(".panel-title a").attr("aria-expanded", true);
+        $(".panel-collapse").addClass("in");
+    }
+
+    // For WORKS Project modal.
     $("a[data-target='#projectModalLG']").on("click", function(ev) {
         ev.preventDefault();
         var target = $(this).attr("href");
@@ -22,4 +26,67 @@ $(document).ready(function() {
             $("#projectModalLG").modal("show");
         });
     });
+
+    $('[data-toggle="tooltip"]').tooltip();
+
+    // For WORK swiper.
+    if ($(".work-swiper").length) {
+	    if (windowWidth <= 991 && windowHeight > windowWidth) {
+	        initialSwiper("small");
+	    } else if (windowWidth <= 991 && windowHeight < windowWidth) {
+	        initialSwiper("bigwidth");
+	    } else {
+	        initialSwiper("bigwidth");
+	    }
+    }
 });
+// $(window).resize(function() {
+//     var windowWidth = $(window).width(),
+//         windowHeight = $(window).height();
+
+//     // For WORK swiper.
+//     if (windowWidth <= 991 && windowHeight > windowWidth) {
+//         initialSwiper("small");
+//     } else if (windowWidth <= 991 && windowHeight < windowWidth) {
+//         initialSwiper("bigwidth");
+//     } else {
+//         initialSwiper("bigwidth");
+//     }
+// });
+
+function initialSwiper(screenType) {
+    if (screenType == "bigwidth") {
+        $(".work-swiper").toggleClass("verticalSwiper");
+        var galleryTop = new Swiper('.gallery-top', {
+            nextButton: '.swiper-button-next',
+            prevButton: '.swiper-button-prev',
+            spaceBetween: 10,
+        });
+        var galleryThumbs = new Swiper('.gallery-thumbs', {
+            spaceBetween: 10,
+            centeredSlides: true,
+            slidesPerView: 'auto',
+            direction: 'vertical',
+            touchRatio: 0.2,
+            slideToClickedSlide: true
+        });
+        galleryTop.params.control = galleryThumbs;
+        galleryThumbs.params.control = galleryTop;
+    } else {
+        $(".work-swiper").toggleClass("horizontalSwiper");
+        var galleryTop = new Swiper('.gallery-top', {
+            nextButton: '.swiper-button-next',
+            prevButton: '.swiper-button-prev',
+            spaceBetween: 10,
+        });
+        var galleryThumbs = new Swiper('.gallery-thumbs', {
+            spaceBetween: 10,
+            centeredSlides: true,
+            slidesPerView: 'auto',
+            touchRatio: 0.2,
+            slideToClickedSlide: true
+        });
+        galleryTop.params.control = galleryThumbs;
+        galleryThumbs.params.control = galleryTop;
+    }
+}
